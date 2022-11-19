9c00668f5628e3f482580aa6c6fe12849741ea00
joeappleton18 <joeappleton18@gmail.com>
2022-11-19 12:04:40 +0000

    made some slight updates to the cypress.config

--- src/hooks/useNextAuth.ts
+++ src/hooks/useNextAuth.ts
@@ -5,7 +5,7 @@ let session = useSession;
 if (process.env.NEXT_PUBLIC_TESTING) {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   //@ts-ignore
-  session = () => ({data: {session: true}});
+  session = () => ({data: {session: true, user: {id: 1}}});
 }
 
 export default session;
